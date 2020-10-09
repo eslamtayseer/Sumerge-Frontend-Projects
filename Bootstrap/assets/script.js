624f1840893d63d620172afe14b6b2268b05dd78
Eslam-Kotb <eslamkotb1@gmail.com>
2020-10-09 18:00:47 +0200

    Finalised Design of checkout page with from validation

--- Bootstrap/assets/script.js
+++ Bootstrap/assets/script.js
@@ -1,3 +1,4 @@
+//calculate total price of order
 function CalculatePrice() {
     var price = 0;
     var elements = document.getElementsByClassName("items")
@@ -12,17 +13,21 @@ function CalculatePrice() {
     window.alert(price)
 
 }
-var btn = document.getElementById("place-order-btn")
-btn.addEventListener("click", CalculatePrice)
-var noItemsBtn = document.getElementsByClassName("number-items")
-    // console.log(noItemsBtn.length)
-for (let index = 0; index < noItemsBtn.length; index++) {
-    const element = noItemsBtn[index];
-    element.addEventListener("", checkInputValue)
 
+//form validation max quantity of an item order cannot exceed 99
+function checkInputValue(event) {
+    var source = event.target || event.srcElement;
+    if (source.value.length > source.maxLength) source.value = source.value.slice(0, source.maxLength);
+    console.log(source.value)
 }
 
-function checkInputValue() {
-    // if (window.Event.sou.value.length > window.EventSource.maxLength) window.EventSource.value = window.EventSource.value.slice(0, window.EventSource.maxLength);
-    console.log("here")
+var btn = document.getElementById("place-order-btn")
+btn.addEventListener("click", CalculatePrice)
+
+
+var noItemsBtn = document.getElementsByClassName("number-items")
+for (let index = 0; index < noItemsBtn.length; index++) {
+    const element = noItemsBtn[index];
+    element.addEventListener("input", checkInputValue)
+
 }
